3f8537a8bc9bf5728f946775764ab7d416008591
tangentwise <frankwisereading@gmail.com>
2026-01-29 14:27:21 -0500

    Add 'DPSTF' to the list of tickers

--- last-mile/script.py
+++ last-mile/script.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 
 # CONFIGURATION
-tickers = ["UPS", "FDX", "EXPD", "AMZN"]
+tickers = ["UPS", "FDX", "EXPD", "AMZN", "DPSTF"]
 folder_name = "last-mile" 
 
 data_dict = {}
